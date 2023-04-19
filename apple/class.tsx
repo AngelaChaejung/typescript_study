1fbf40cae918bd2b98e9f669c58caf8fb5e0591f
Angelachaejung <a01083298599@gmail.com>
2023-04-19 22:28:55 +0900

    Create class.tsx

---
+++ apple/class.tsx
@@ -0,0 +1,16 @@
+function 기계(여기) {
+  this.q = 여기; //새로생성된object의 q
+  this.w = "snowball";
+}
+
+let nunu = new 기계("consume");
+let garen = new 기계("strike");
+
+console.log(기계);
+
+class Hero {
+  constructor(여기) {
+    this.q = 여기; //새로생성된object의 q
+    this.w = "snowball";
+  }
+}
